8996300cbc6d79903876e518dff758d9f59cc09b
zong4 <2481814748@qq.com>
2023-07-17 15:44:24 +0800

    add move

--- memory/copy.cpp
+++ memory/copy.cpp
@@ -69,3 +69,53 @@ int main()
 
     return 0;
 }
+
+// vec.emplace_back()
+// default constructor A
+
+// vec.emplace_back(0)
+// constructor A
+
+// vec.emplace_back(A())
+// default constructor A
+// move constructor A
+// deconstructor A
+
+// vec.push_back(A())
+// default constructor A
+// move constructor A
+// deconstructor A
+
+// A a
+// default constructor A
+
+// vec.emplace_back(a)
+// copy constructor A
+
+// vec.push_back(a)
+// copy constructor A
+// deconstructor A
+
+// vec.emplace_back()
+// default constructor A
+// copy constructor A
+// copy constructor A
+// copy constructor A
+// copy constructor A
+// copy constructor A
+// copy constructor A
+// deconstructor A
+// deconstructor A
+// deconstructor A
+// deconstructor A
+// deconstructor A
+// deconstructor A
+
+// -----------------------------------
+// deconstructor A
+// deconstructor A
+// deconstructor A
+// deconstructor A
+// deconstructor A
+// deconstructor A
+// deconstructor A
--- std/cpp14/move.cpp
+++ std/cpp14/move.cpp
@@ -1,15 +1,67 @@
 #include <iostream>
+#include <memory>
+#include <vector>
 
-void Add(const int&& a, const int&& b)
+class A
 {
-    std::cout << a + b << std::endl;
-}
+public:
+    int _val = 1;
+
+public:
+    A() { std::cout << "A()" << std::endl; }
+    A(const A&) = delete;
+    A(A&&)      = delete;
+    ~A() { std::cout << "~A()" << std::endl; }
+
+    A& operator=(const A&) = delete;
+    A& operator=(A&&)      = delete;
+};
 
 int main()
 {
-    const int a = 1;
-    const int b = 2;
-    Add(std::move(a), std::move(b));
+    // unique pointer
+    {
+        std::unique_ptr<int> p1(new int(5));
+
+        // std::unique_ptr<int> p2 = p1; // error, can't copy unique_ptr
+        std::unique_ptr<int> p3 = std::move(p1); // ok, p3 is now the owner of the memory
+
+        // std::cout << *p1 << std::endl;
+        std::cout << *p3 << std::endl;
+
+        auto fun = [p3 = std::move(p3)]() { std::cout << *p3 << std::endl; };
+        fun();
+        // std::cout << *p3 << std::endl; // unqiue_ptr isn't move back
+    }
+
+    // raw pointer
+    {
+        int* p4 = new int(10);
+        int* p5 = std::move(p4); // equal to p5 = p4
+        std::cout << *p4 << std::endl;
+    }
+
+    // shared pointer
+    {
+        std::shared_ptr<int> p6(new int(20));
+        std::shared_ptr<int> p7 = std::move(p6);
+        // std::cout << *p6 << std::endl; // change ownership
+        std::cout << *p7 << std::endl;
+    }
+
+    // class A
+    {
+        A a1;
+        // A a2 = std::move(a1);
+    }
+
+    // vector
+    {
+        std::vector<int*> v1{new int(1), new int(2), new int(3)};
+        std::vector<int*> v2 = std::move(v1);
+        // std::cout << *v1[0] << std::endl; // change ownership
+        std::cout << *v2[0] << std::endl;
+    }
 
     return 0;
 }
